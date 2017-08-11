6b49d11a4050897dfcdacd221c6f8288de7ebd14
Shiva Kaushik Kalahasti <kaushik.7689@gmail.com>
2017-08-11 14:31:25 -0600

    feat(module-api): add spring cors support

--- api/src/main/java/uc/cs/Application.java
+++ api/src/main/java/uc/cs/Application.java
@@ -2,13 +2,22 @@ package uc.cs;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @ComponentScan
 @EnableWebMvc
-public class Application{
+public class Application extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("//").allowedOrigins("http://mocker.egen.io","http://localhost:8080/*")
+                .allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS")
+                .allowedHeaders("header1", "header2", "header3")
+                .exposedHeaders("header1", "header2")
+                .allowCredentials(false).maxAge(3600);
 
     }
 }
